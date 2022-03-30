bf095090141c53698cbd34d5686370958b579061
Athira612 <74651653+Athira612@users.noreply.github.com>
2022-03-30 12:27:03 +0530

    main exam

---
+++ exam/athira 1.py
@@ -0,0 +1,8 @@
+def absent_digits(n):
+  all_nums = set([0,1,2,3,4,5,6,7,8,9])
+  n = set([int(i) for i in n])
+  n = n.symmetric_difference(all_nums)
+  n = sorted(n)
+  return n
+print()
+print(absent_digits([9,6,1,1,6,7,7,0,2]))
